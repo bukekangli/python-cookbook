2cb1250fa105f65df51bd1461d3730c905216197
商力亢 <shanglikang@qiushibaike.com>
2019-03-22 10:25:23 +0800

    Features:熟悉了python实现定时任务的3中方法。

--- timer/apscheduler/apscheduler_test.py
+++ timer/apscheduler/apscheduler_test.py
@@ -1,23 +1,25 @@
-import time
-from pytz import utc
-from datetime import datetime
+import os
+import sys
+from datetime import datetime, timedelta
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-
-
-
-def job():
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-
-
-def job2():
-    print(int(time.time()))
+def alarm(time):
+    print('Alarm! This is alarm was scheduled at %s' % time)
 
 
 if __name__ == '__main__':
-
     sched = BlockingScheduler()
-    sched.add_job(job, 'interval', seconds=3)
-    sched.add_job(job2, 'interval', seconds=1)
-    sched.start()
+    sched.add_jobstore('redis')
+    if len(sys.argv) > 1 and sys.argv[1] == '--clear':
+        sched.remove_all_jobs()
+    alarm_time = datetime.now() + timedelta(seconds=1)
+    sched.add_job(alarm, 'date', run_date=alarm_time, args=[datetime.now()])
+    sched.add_job(alarm, 'interval', seconds=1)
+    print('To clear the alarms, run this example with the --clear argument.')
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+    try:
+        sched.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
